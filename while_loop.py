40d432b4314fc6eda462fa7efd11b03aae56935c
Jonathan Davies <jonathan.davies@dci-student.org>
2023-10-25 12:45:01 +0200

    While_loop_updated

---
+++ while_loop.py
@@ -0,0 +1,12 @@
+amount: int = 0 
+
+while amount < 10: 
+	#instructions 
+	print(amount)
+	amount += 3
+#nested
+for items in items:
+	for number in numbers:
+		#set of instructions
+		# sets of instructions	
+		
